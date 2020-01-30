a54f99799f89efa91d42dc7d90bfd6b166c48733
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-01-30 12:57:11 +0200

    last commit fix

--- back/src/middlewares/updateLastSeen.ts
+++ back/src/middlewares/updateLastSeen.ts
@@ -1,9 +1,15 @@
+import express from "express";
+
 import { UserModel } from "../models";
 
-export default (req, res, next) => {
-  UserModel.updateOne({
-    _id: "5e3147e39104f029583a0ea8",
-    last_seen: new Date()
-  });
+export default (req: express.Request, res: express.Response, next: any) => {
+  UserModel.findOneAndUpdate(
+    { _id: "5e3147e39104f029583a0ea8" },
+    {
+      last_seen: new Date()
+    },
+    { new: true },
+    () => {}
+  );
   next();
 };
